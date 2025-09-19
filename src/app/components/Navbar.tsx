f93628807dd2ceff2be77be8fcc07ffee6c3cc4c
edwin satya <edwinsatyayudistira@gmail.com>
2025-09-19 10:56:18 +0700

    remove margin bottom in navbar

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link'
 
 export default function Navbar(){
   return (
-    <nav className="bg-blue-500 p-4 mb-6">
+    <nav className="bg-blue-500 p-4">
       <div className="max-w-7xl mx-auto flex justify-center items-center">
         <Link href="/" className="text-white text-2xl font-bold">
           Product Showcase
